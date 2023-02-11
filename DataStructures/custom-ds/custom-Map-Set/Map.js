abccd8cbf81f60d29ff75477396c4a1602e5cef8
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 11:17:06 +0600

    custom map implementation

---
+++ DataStructures/custom-ds/custom-Map-Set/Map.js
@@ -0,0 +1,47 @@
+class Map{
+    #size = 0;
+    constructor() {
+        this.collection = {};
+    }
+
+    size() {
+        return this.#size;
+    }
+
+    add(key, value) {
+        this.collection[key] = value;
+        this.#size++;
+    }
+
+    remove(key) {
+        if(this.has(key)) {
+            delete this.collection[key];
+            this.#size--;
+        }
+    }
+
+    has(key) {
+        return this.collection[key] !== undefined;
+    }
+
+    get(key) {
+        if(this.has(key)) {
+            return this.collection[key];
+        }
+    }
+
+    values() {
+        return Object.values(this.collection);
+    }
+
+    clear() {
+        this.collection = {};
+        this.#size = 0;
+    }
+}
+
+const map = new Map()
+map.add("a", "abc");
+map.add("b", "bcd");
+
+console.log(map);
