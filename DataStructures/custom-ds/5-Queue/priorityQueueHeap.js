9e2960f53b40b0665e63669468efdd13524fdc92
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 09:20:57 +0600

    priority queue

--- DataStructures/custom-ds/5-Queue/priorityQueueHeap.js
+++ DataStructures/custom-ds/5-Queue/priorityQueueHeap.js
@@ -1,59 +1,54 @@
 /*
-!!!!!!!!!!!!!!!!!!!!!!completed
+Priority Queue(Min Heap Implementation)
+
+    It can be implemented using other Data Structures but is commonly implemented using a Heap.
+
+    Each node contains a priority. When we enqueue a node to the queue, it’s “bubbled up” to its place in the queue.
+
+    Time Complexity:
+        enqueue() - O()
+        dequeue() - O()
 */
-class Queue {
-    constructor() {
-        this.items = {};
-        this.rear = 0;
-        this.front = 0;
-    }
 
-    enqueue(element) { 
-        this.items[this.rear] = element;
-        this.rear++;
-    }
-
-    dequeue() {
-        if(!this.isEmpty()) {
-            const item =  this.items[this.front]
-            delete this.items[this.front];
-            this.front++;
-            return item;
+    class PriorityQueue {
+        constructor() {
+            this.collection = [];
         }
-        return "Queue is empty!!";
-    }
-
-    peek() {
-        if(!this.isEmpty()) {
-            return this.items[this.front];
+    
+        printCollection = function () {
+            console.log(this.collection);
         }
-        return null;
+    
+        size() {
+            return this.collection.length;
+        }
+    
+        isEmpty() {
+            return this.size() === 0;
+        }
+    
+        enqueue(item) {
+            let index = this.collection.findIndex(elem => elem[1] > item[1]);
+            if (index !== -1) {
+                this.collection.splice(index, 0, item);
+            } else {
+                this.collection.push(item);
+            }
+        }
+    
+        dequeue() {
+            if (!this.isEmpty()) return this.collection.shift()[0];
+        }
+    
+        front() {
+            if (!this.isEmpty()) return this.collection[0][0];
+        }
     }
-
-    isEmpty() {
-        return this.rear - this.front === 0;
-    }
-
-    size() {
-        return this.rear - this.front;
-    }
-
-    print() {
-        console.log(this.items);
-    }
-}
-
-
-const queue = new Queue();
-
-console.log(queue.isEmpty());
-
-queue.enqueue(20);
-queue.enqueue(10);
-queue.enqueue(30);
-console.log(queue.size());
-queue.print();
-
-console.log(queue.dequeue());
-console.log(queue.peek());
-queue.print();
+    
+    const queue = new PriorityQueue();
+    
+    queue.enqueue(["BBB", 3]);
+    queue.enqueue(["AAA", 1]);
+    queue.enqueue(["ABB", 3]);
+    queue.enqueue(["AAB", 2]);
+    queue.enqueue(["AB", 2]);
