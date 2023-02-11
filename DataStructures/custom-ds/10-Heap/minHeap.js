515d07f0bc4361873f0f276381aa1732899f9463
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 09:57:49 +0600

    Min Heap

---
+++ DataStructures/custom-ds/10-Heap/minHeap.js
@@ -0,0 +1,105 @@
+class minHeap {
+    constructor() {
+        this.heap = new Array();
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    parent = (index) => {
+        return Math.floor((index - 1) / 2);
+    }
+
+    swap(index1, index2) {
+        const elem = this.heap[index1];
+        this.heap[index1] = this.heap[index2];
+        this.heap[index2] = elem;
+    }
+
+    insert = (element) => {
+        this.heap.push(element);
+        this.heapifyUp(this.heap.length - 1);
+    }
+
+    heapifyUp = (index) => {
+        let currentIndex = index;
+        let parentIndex = this.parent(currentIndex);
+
+        while (currentIndex > 0 && this.heap[currentIndex] < this.heap[parentIndex]) {
+            this.swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = this.parent(parentIndex);
+        }
+    }
+
+    heapify(i) {
+        let smallest = i;
+        let l = 2 * i + 1;
+        let r = 2 * i + 2;
+        let length = this.size();
+
+        if (l < length && this.heap[l] < this.heap[smallest]) {
+            smallest = l;
+        }
+
+        if (r < length && this.heap[r] < this.heap[smallest]) {
+            smallest = r;
+        }
+
+        if (smallest != i) {
+            this.swap(i, smallest);
+            this.heapify(smallest);
+        }
+    }
+
+    remove() {
+        let max = this.heap[0];
+        let last = this.heap.pop();
+        this.heap[0] = last;
+        this.heapify(0);
+        return max;
+    }
+
+
+    removeValue(value) {
+        if (!value) return;
+
+        let i;
+        let size = this.size();
+
+        for (i = 0; i < size; i++) {
+            if (value === this.heap[i]) break;
+        }
+
+        this.swap(i, size - 1);
+        this.heap.length--;
+
+        // Build Min Heap
+        for (let j = Math.floor(size / 2) - 1; j >= 0; j--) {
+            this.heapify(j);
+        }
+    }
+
+    printHeap() {
+        let txt = "";
+        for (let i = 0; i < this.size(); i++) {
+            txt += this.heap[i] + " ";
+        }
+        process.stdout.write(txt + "\n");
+    }
+};
+
+const heap = new minHeap();
+heap.insert(5);
+heap.insert(7);
+heap.insert(9);
+heap.insert(11);
+heap.insert(13);
+heap.insert(16);
+heap.insert(18);
+heap.insert(15);
+heap.removeValue(5);
+heap.removeValue(7);
+
+heap.printHeap();
--- DataStructures/custom-ds/5-Queue/priorityQueueHeap.js
+++ DataStructures/custom-ds/5-Queue/priorityQueueHeap.js
@@ -8,46 +8,87 @@ Priority Queue(Min Heap Implementation)
         enqueue() - O()
         dequeue() - O()
 */
+class Task {
+    constructor(task, priority) {
+        this.task = task;
+        this.priority = priority;
+    }
+}
 
-    class PriorityQueue {
-        constructor() {
-            this.collection = [];
-        }
-    
-        printCollection = function () {
-            console.log(this.collection);
-        }
-    
-        size() {
-            return this.collection.length;
-        }
-    
-        isEmpty() {
-            return this.size() === 0;
-        }
-    
-        enqueue(item) {
-            let index = this.collection.findIndex(elem => elem[1] > item[1]);
-            if (index !== -1) {
-                this.collection.splice(index, 0, item);
-            } else {
-                this.collection.push(item);
-            }
-        }
-    
-        dequeue() {
-            if (!this.isEmpty()) return this.collection.shift()[0];
-        }
-    
-        front() {
-            if (!this.isEmpty()) return this.collection[0][0];
+class PriorityQueue {
+    constructor(capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.heap = new Array(capacity);
+    }
+
+    size() {
+        return this.size;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    isFull() {
+        return this.size === this.capacity;
+    }
+
+    parentIndex (index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    swap(index1, index2) {
+        let task = this.heap[index1];
+        this.heap[index1] = this.heap[index2];
+        this.heap[index2] = task;
+    }
+
+    heapifyUp(index) {
+        let currentIndex = index;
+        let children = this.heap[index];
+        let parent = this.heap[this.parentIndex(index)];
+
+        while(currentIndex < this.size && children.priority < parent.priority) {
+
         }
     }
-    
-    const queue = new PriorityQueue();
-    
-    queue.enqueue(["BBB", 3]);
-    queue.enqueue(["AAA", 1]);
-    queue.enqueue(["ABB", 3]);
-    queue.enqueue(["AAB", 2]);
-    queue.enqueue(["AB", 2]);
+
+    enqueue(item, priority) {
+        if(this.isFull()) {
+            process.stdout.write("Queue is full!");
+        } else {
+            const newTask = new Task(item, priority);
+            this.heap[this.size++] = newTask;
+            heapifyUp(this.size - 1);
+        }
+    }
+
+    dequeue() {
+        if (!this.isEmpty()) return this.collection.shift()[0];
+    }
+
+    front() {
+        if (!this.isEmpty()) return this.collection[0][0];
+    }
+
+    printQueue() {
+        if (isEmpty()) {
+            process.stdout.write("Queue is empty!");
+        } else {
+            for (let i = 0; i < heapSize; i++) {
+                process.stdout.write((i + 1) + " " + heap[i].task + " (priority " + heap[i].priority + ")\n");
+            }
+        }
+        process.stdout.write("\n");
+    }
+}
+
+const queue = new PriorityQueue();
+queue.enQueue("Coding", 3);
+queue.enQueue("Sleeping", 2);
+queue.enQueue("Music", 1);
+queue.enQueue("Being sad for no reason for a while", 7);
+queue.enQueue("Tv Series", 5);
+
+queue.printQueue();
