250bbd659378a82014fd1a07c17f93383415ccec
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 08:56:49 +0600

    linkedlist stack

--- DataStructures/custom-ds/4-Stack/linkedList-stack.js
+++ DataStructures/custom-ds/4-Stack/linkedList-stack.js
@@ -1,4 +1,4 @@
-const LinkedList = require('../6-LinkedList/linkedList-withTailPointer');
+const { LinkedList } = require('../6-LinkedList/linkedList-withTailPointer');
 
 class LinkedListStack {
     constructor() {
