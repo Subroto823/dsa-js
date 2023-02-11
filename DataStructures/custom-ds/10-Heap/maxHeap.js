bfbbd03af4bfcde7e617991f842f2db3cfd8eb94
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 07:50:50 +0600

    Max Heap

--- DataStructures/custom-ds/10-Heap/maxHeap.js
+++ DataStructures/custom-ds/10-Heap/maxHeap.js
@@ -1,17 +1,24 @@
-const MaxHeap = function () {
-    this.heap = [];
-    this.parent = (index) => {
+class MaxHeap {
+    constructor() {
+        this.heap = new Array();
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    parent = (index) => {
         return Math.floor((index - 1) / 2);
     }
 
-    this.insert = (element) => {
+    insert = (element) => {
         this.heap.push(element);
         this.heapifyUp(this.heap.length - 1);
     }
 
-    this.heapifyUp = index => {
-        let currentIndex = index,
-            parentIndex = this.parent(currentIndex);
+    heapifyUp = (index) => {
+        let currentIndex = index;
+        let parentIndex = this.parent(currentIndex);
 
         while (currentIndex > 0 && this.heap[currentIndex] > this.heap[parentIndex]) {
             this.swap(currentIndex, parentIndex);
@@ -20,11 +27,13 @@ const MaxHeap = function () {
         }
     }
 
-    this.swap = (index1, index2) => {
-        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    swap(index1, index2) {
+        const elem = this.heap[index1];
+        this.heap[index1] = this.heap[index2];
+        this.heap[index2] = elem;
     }
 
-    this.remove = () => {
+    remove() {
         let max = this.heap[0];
         let last = this.heap.pop();
         this.heap[0] = last;
@@ -32,11 +41,30 @@ const MaxHeap = function () {
         return max;
     }
 
-    this.heapify = (i) => {
+
+    removeValue(value) {
+        if (!value) return;
+
+        let i;
+        let size = this.size();
+
+        for (i = 0; i < size; i++) {
+            if (value === this.heap[i]) break;
+        }
+
+        this.swap(i, size - 1);
+        this.heap.length--;
+
+        for (let j = 0; j < Math.floor(size / 2) - 1; j++) {
+            this.heapify(j);
+        }
+    }
+
+    heapify(i) {
         let large = i;
         let l = 2 * i + 1;
         let r = 2 * i + 2;
-        let length = this.heap.length - 1;
+        let length = this.size();
 
         if (l < length && this.heap[l] > this.heap[large]) {
             large = l;
@@ -52,20 +80,24 @@ const MaxHeap = function () {
         }
     }
 
-    this.print = () => {
-        return this.heap;
+    printHeap() {
+        let txt = "";
+        for (let i = 0; i < this.size(); i++) {
+            txt += this.heap[i] + " ";
+        }
+        process.stdout.write(txt + "\n");
     }
-
 };
 
 const maxHeap = new MaxHeap();
-// maxHeap.insert(5);
-// maxHeap.insert(7);
-// maxHeap.insert(9);
-// maxHeap.insert(11);
-// maxHeap.insert(13);
-// maxHeap.insert(16);
-// maxHeap.insert(18);
-// maxHeap.remove();
+maxHeap.insert(5);
+maxHeap.insert(7);
+maxHeap.insert(9);
+maxHeap.insert(11);
+maxHeap.insert(13);
+maxHeap.insert(16);
+maxHeap.insert(18);
+maxHeap.insert(15);
+maxHeap.removeValue(18);
 
-console.log(maxHeap.print());
+maxHeap.printHeap();
