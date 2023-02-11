18fddc6c7715875cd4ffc8cfc56583394b581ade
subroto823 <subroto.sarker.823@gmail.com>
2023-02-11 08:40:22 +0600

    Heap Sort

---
+++ Algorithms/BasicAlgorithms/SortingAlgorithm/heapSort.js
@@ -0,0 +1,55 @@
+/* 
+heapSort - descending order (using minHeap)
+
+    Time Complexity: O(nlogn)
+    Space Complexity: O(1)
+
+*/
+
+function swap (arr, index1, index2) {
+    let tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+}
+
+function buildMinHeap(arr, size) {
+    for(let i = Math.floor(size/2) - 1; i >= 0; i--) {
+        heapify(arr, size, i);
+    }
+}
+
+function heapify(arr, size, i) {
+    let smallest = i;
+    let l = (2 * i) + 1;
+    let r = (2 * i) + 2;
+
+    if(l < size && arr[l] < arr[smallest]) {
+        smallest = l
+    }
+
+    if(r < size && arr[r] < arr[smallest]) {
+        smallest = r;
+    }
+
+    if(smallest != i) {
+        swap(arr, i, smallest);
+        heapify(arr, size, smallest);
+    }
+}
+
+function heapSort(arr) {
+    let len = arr.length;
+
+    // build MaxHeap
+    buildMinHeap(arr, len);
+
+    // heapSort
+    for(let i = len - 1; i >= 0; i--) {
+        swap(arr, i, 0);
+        heapify(arr, i, 0);
+    }
+}
+
+let arr = [4, 6, 3, 7, 9, 12, 5, 2, 1, 8, 11, 10];
+heapSort(arr);
+console.log(arr);
--- DataStructures/custom-ds/10-Heap/heapSort.js
+++ DataStructures/custom-ds/10-Heap/heapSort.js
@@ -1,10 +1,23 @@
+/* 
+heapSort - ascending order (using maxHeap)
+
+    Time Complexity: O(nlogn)
+    Space Complexity: O(1)
+
+*/
+
 function swap (arr, index1, index2) {
     let tmp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = tmp;
 }
 
-// max Heap
+function buildMaxHeap(arr, size) {
+    for(let i = Math.floor(size/2) - 1; i >= 0; i--) {
+        heapify(arr, size, i);
+    }
+}
+
 function heapify(arr, size, i) {
     let largest = i;
     let l = (2 * i) + 1;
@@ -19,7 +32,24 @@ function heapify(arr, size, i) {
     }
 
     if(largest != i) {
-        swap(i, largest);
+        swap(arr, i, largest);
         heapify(arr, size, largest);
     }
 }
+
+function heapSort(arr) {
+    let len = arr.length;
+
+    // build MaxHeap
+    buildMaxHeap(arr, len);
+
+    // heapSort
+    for(let i = len - 1; i >= 0; i--) {
+        swap(arr, i, 0);
+        heapify(arr, i, 0);
+    }
+}
+
+let arr = [4, 6, 3, 7, 9, 12, 5, 2, 1, 8, 11, 10];
+heapSort(arr);
+console.log(arr);
