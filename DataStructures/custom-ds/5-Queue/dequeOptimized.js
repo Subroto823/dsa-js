9f9b31831d5c24637e33c29817288c0af926dc00
subroto823 <subroto.sarker.823@gmail.com>
2023-02-13 11:38:04 +0600

    Optimized Deque - Circular Deque

--- DataStructures/custom-ds/5-Queue/dequeOptimized.js
+++ DataStructures/custom-ds/5-Queue/dequeOptimized.js
@@ -1,4 +1,6 @@
 /*
+Optimized Deque - Circular Deque
+
 Deque or Double Ended Queue is a type of queue in which insertion and removal of elements can either be performed from the front or the rear. 
 Thus, it does not follow FIFO rule (First In First Out).
 
