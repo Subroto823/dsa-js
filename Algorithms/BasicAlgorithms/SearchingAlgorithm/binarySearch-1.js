082576296539e9bc8b5749ca837ce7ca6d831be4
subroto823 <subroto.sarker.823@gmail.com>
2023-02-18 19:32:56 +0600

    binary Search

---
+++ Algorithms/BasicAlgorithms/SearchingAlgorithm/binarySearch-1.js
@@ -0,0 +1,27 @@
+/**
+ * @Binary Search
+ * Time complexity O(logn)
+ * (only works for sorted array)
+ */ 
+
+const binarySearch = (arr, target) => {
+    let start = 0;
+    let end = arr.length;
+
+    while(start < end) {
+        let mid = (start + end) >> 1;
+
+        if(target === arr[mid]) {
+            return mid;
+        }
+        if(target < arr[mid]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+
+console.log(binarySearch([-5, 2, 4, 6, 20], 20));
---
+++ Algorithms/BasicAlgorithms/SearchingAlgorithm/binarySearch-2.js
@@ -0,0 +1,29 @@
+/**
+ * @Binary Search
+ * Time complexity O(logn)
+ * (only works for sorted array)
+ * (This implementation uses fewer number of comparisons than the previous one)
+ */ 
+
+const binarySearch = (arr, target) => {
+    let start = 0;
+    let end = arr.length - 1;
+
+
+    while(end - start > 1) {
+        let mid = start + Math.floor((end - start) / 2);
+        
+        if(target <= arr[mid]) {
+            end = mid;
+        } else {
+            start = mid;
+        }
+    }
+
+    if(arr[start] === target) return start;
+    else if(arr[end] === target) return end;
+    else return -1;
+}
+
+
+console.log(binarySearch([-5, 2, 4, 6, 20], 20));
