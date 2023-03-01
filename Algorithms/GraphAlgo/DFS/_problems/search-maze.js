77343b044771d0f66cdab8bef75400ccf1d0d8f2
subroto823 <subroto.sarker.823@gmail.com>
2023-03-01 09:14:08 +0600

    graph - search maze

--- Algorithms/GraphAlgo/DFS/_problems/search-maze.js
+++ Algorithms/GraphAlgo/DFS/_problems/search-maze.js
@@ -7,8 +7,36 @@
 */
 
 var hasPath = function(maze, start, destination) {
+    maze[start[0]][start[1]] = 1;
+    return searchMazeHelper(maze, start, destination);
 
 };
+
+function searchMazeHelper(maze, start, destination) {
+    if(start[0] === destination[0] && start[1] === destination[1]) {
+        return true;
+    }
+
+    let neighborIndices;
+    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // top, right, bottom, left
+
+    for(let direction of directions) {
+        neighborIndices = [start[0]+direction[0], start[1]+direction[1]];
+
+        if(isFeasible(maze, neighborIndices)) {
+            maze[neighborIndices[0]][neighborIndices[1]] = 1;
+            if(searchMazeHelper(maze, neighborIndices, destination)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function isFeasible(maze, indices) {
+    let x = indices[0], y = indices[1];
+    return x >= 0 && y >= 0 && x < maze.length && y < maze[x].length && maze[x][y] !== 1;
+}
 
 var maze = [
     [0, 0, 1, 0, 0],
