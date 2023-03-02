4a51290506292a2033bc837ca22b3a2d66c751cb
subroto823 <subroto.sarker.823@gmail.com>
2023-03-02 06:01:31 +0600

    merge sort - bottom up

--- Algorithms/BasicAlgorithms/SortingAlgorithm/mergeSort-iv.js
+++ Algorithms/BasicAlgorithms/SortingAlgorithm/mergeSort-iv.js
@@ -8,37 +8,36 @@
     # This implementation is more optimal than the previous two
  */
 
-    const { randomArray } = require("./helper/randomNumbers");
+const { randomArray } = require("./helper/randomNumbers");
 
-    function sortArray(nums) {
-        let merged = new Array(arr.length).fill(0);
-        mergeSort(nums, merged, 0, arr.length - 1);
-    }
-    
-    function mergeSort(arr, merged, lo, hi) {
-        if (lo < hi) {
-            let mid = (lo + hi) >> 1;
-            mergeSort(arr, merged, lo, mid);
-            mergeSort(arr, merged, mid + 1, hi);
-            merge(arr, merged, lo, mid, hi);
+function mergeSort(nums) {
+    const n = nums.length;
+    const merged = new Array(n).fill(0);
+
+    for (let i = 1; i < n; i *= 2) {
+        for (let lo = 0; lo < n - i; lo += (i * 2)) {
+            let mid = lo + i - 1;
+            let hi = Math.min(lo + (2 * i) - 1, n - 1);
+            merge(nums, merged, lo, mid, hi);
         }
     }
-    
-    function merge(nums, merged, lo, mid, high) {
-    
-        let i = lo, j = mid + 1;
-        for (let k = lo; k <= high; k++) {
-            if (i > mid) merged[k] = nums[j++];
-            else if (j > high) merged[k] = nums[i++];
-            else if (nums[j] < nums[i]) merged[k] = nums[j++];
-            else merged[k] = nums[i++];
-        }
-    
-        for (let k = lo; k <= high; k++) {
-            nums[k] = merged[k];
-        }
+}
+
+function merge(nums, merged, lo, mid, high) {
+
+    let i = lo, j = mid + 1;
+    for (let k = lo; k <= high; k++) {
+        if (i > mid) merged[k] = nums[j++];
+        else if (j > high) merged[k] = nums[i++];
+        else if (nums[j] < nums[i]) merged[k] = nums[j++];
+        else merged[k] = nums[i++];
     }
-    
-    let arr = randomArray(20, 40);
-    sortArray(arr);
-    console.log(arr);
+
+    for (let k = lo; k <= high; k++) {
+        nums[k] = merged[k];
+    }
+}
+
+let arr = randomArray(20, 40);
+mergeSort(arr);
+console.log(arr);
