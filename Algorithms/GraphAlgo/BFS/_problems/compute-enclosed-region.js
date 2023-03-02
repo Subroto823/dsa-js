5dad1d5b53fbb43ceaceb5c4c957ca149907bba2
subroto823 <subroto.sarker.823@gmail.com>
2023-03-02 07:58:39 +0600

    graph - enclosed region

--- Algorithms/GraphAlgo/BFS/_problems/compute-enclosed-region.js
+++ Algorithms/GraphAlgo/BFS/_problems/compute-enclosed-region.js
@@ -14,6 +14,59 @@ function fillSurroundedRegions(board) {
         .fill()
         .map(() => new Array(cols).fill(false));
 
+    for(let i = 0; i < rows; i++) {
+        if(board[i][0] === "W" && !visited[i][0]) {
+            markBoundaryRegion(board, visited,i, 0);
+        }
+        if(board[i][rows-1] === "W" && !visited[i][rows-1]) {
+            markBoundaryRegion(board, visited,i, rows-1);
+        }
+    }
+
+    for(let i = 0; i < cols-1; i++) {
+        if(board[0][i] === "W" && !visited[0][i]) {
+            markBoundaryRegion(board, visited, 0, i);
+        }
+        if(board[rows-1][i] === "W" && !visited[rows-1][i]) {
+            markBoundaryRegion(board, visited, rows-1, i);
+        }
+    }
+
+    for(let i = 1; i < rows - 1; i++) {
+        for(let j = 1; j < rows-1; j++) {
+            if(board[i][j] === 'W' && !visited[i][j]) {
+                board[i][j] = 'B';
+            }
+        }
+    }
+    return board;
+}
+
+function markBoundaryRegion(board, visited, x, y) {
+    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    const queue = [];
+    queue.push([x, y]);
+    visited[x][y] = true;
+    let currentPosition, neighbor;
+
+    while(queue.length) {
+        currentPosition = queue.shift();
+
+        for(let direction of directions) {
+            neighbor = [x + direction[0], y + direction[1]];
+            if(isFeasible(board, visited, neighbor)) {
+                visited[neighbor[0]][neighbor[1]] = true;
+                queue.push(neighbor);
+            }
+        }
+    }
+}
+
+function isFeasible(board, visited, neighbor) {
+    let x = neighbor[0];
+    let y = neighbor[1];
+
+    return x >= 0 && x < board.length && y >= 0 && y < board[x].length && !visited[x][y] && board[x][y] === 'W';
 }
 
 var board = [
@@ -22,7 +75,18 @@ var board = [
     ['B','W','W','B'],
     ['B','B','B','B']
 ];
-fillSurroundedRegions(board);
 
+fillSurroundedRegions(board);
+console.table(board);
+
+board = [
+    ['B','B','W','B'],
+    ['W','B','W','W'],
+    ['B','W','B','B'],
+    ['B','B','W','B']
+];
+
+fillSurroundedRegions(board);
+console.table(board);
 /*
 Time Complexity: O(mn) */
