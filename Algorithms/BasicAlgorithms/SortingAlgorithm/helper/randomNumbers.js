8edf5099e1c336e37e9c32418850ab6fc874c980
subroto823 <subroto.sarker.823@gmail.com>
2023-03-04 11:52:26 +0600

    random array

--- Algorithms/BasicAlgorithms/SortingAlgorithm/helper/randomNumbers.js
+++ Algorithms/BasicAlgorithms/SortingAlgorithm/helper/randomNumbers.js
@@ -1,8 +1,18 @@
 function randomArray(length, max = length) {
     // ~~ is a faster substitute for Math.floor() for positive numbers
-    return Array.from({length: length}, () => ~~(Math.random() * max));
+    return Array.from({ length: length }, () => ~~(Math.random() * max));
+}
+
+function randomNumbers(length, min = 0, max = length) {
+    return Array.from({ length: length }, () => {
+        let maxNum = ~~(Math.random() * max);
+        let minNum = Math.floor(Math.random() * min);
+        return maxNum & 1 ? minNum : maxNum;
+    }
+    );
 }
 
 module.exports = {
-    randomArray
+    randomArray,
+    randomNumbers
 }
