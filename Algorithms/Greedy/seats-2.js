c95d659803e6bdc5bf166440e5c9c6210a7f8242
subroto823 <subroto.sarker.823@gmail.com>
2023-03-26 19:36:11 +0600

    seats

--- Algorithms/Greedy/seats-2.js
+++ Algorithms/Greedy/seats-2.js
@@ -1,4 +1,6 @@
 /*
+(Optimal Solution)
+
 Problem Discussion
 
 There is a row of empty (.) and filled (x) seats. Find the minimum number of moves required to make the people sit together. 
@@ -16,30 +18,32 @@ Constraints:
 
 function seats(A) {
     let MOD = 10000003;
-    let n = A.length;
-    if(n === 0) return 0;
-
     let crosses = [];
 
     // all the indices of x's
-    for(let i = 0; i < n; i++) {
-        if(A[i] === "x") {
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] === "x") {
             crosses.push(i);
         }
     }
     // move req assuming starting position is 0
     crosses = crosses.map((elem, i) => elem - i);
 
+    let n = crosses.length;
+    if(n === 0) return 0;
+
     let ans = Infinity;
-    for(let i = 0; i < n; i++) {
-        let total = 0;
-        for(let cross of crosses) {
-            total += Math.abs(cross - i);
-            total %= MOD;
-        }
-        ans = Math.min(ans, total % MOD);
+    let segmentStart = crosses[~~(n / 2)];
+
+    let total = 0;
+    for (let cross of crosses) {
+        total += Math.abs(cross - segmentStart);
+        total %= MOD;
     }
+    ans = Math.min(ans, total % MOD);
+
     return ans;
 }
 
 console.log(seats("..x..x."));
+console.log(seats("xx..xx....xxx"));
