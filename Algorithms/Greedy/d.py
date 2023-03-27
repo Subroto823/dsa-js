65efbbac2ba0b5dcc4f527aff5f4fbfd76377069
subroto823 <subroto.sarker.823@gmail.com>
2023-03-27 20:00:31 +0600

    greedy algo

--- Algorithms/Greedy/d.py
+++
@@ -1,9 +0,0 @@
-def seats(A):
-    MOD = 10000003
-
-    crosses = [i for i, c in enumerate(A) if c == "x"]
-    print(crosses)
-    crosses = [(cross-i) for i, cross in enumerate(crosses)]
-    print(crosses)
-
-seats('..x..x.')
