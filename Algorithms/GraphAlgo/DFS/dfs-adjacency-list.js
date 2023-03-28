4019490e33f37b81bca951ff2fce662244aaaf74
subroto823 <subroto.sarker.823@gmail.com>
2023-03-28 17:39:10 +0600

    depth first search - recursive

--- Algorithms/GraphAlgo/DFS/dfs-adjacency-list.js
+++ Algorithms/GraphAlgo/DFS/dfs-adjacency-list.js
@@ -1,41 +1,44 @@
 /**
-* 
-* @DFS - Depth First Search (in adjacency List)
-*
-* @Time Complexity: O(V+E)
-* @Space Complexity: O(V+E)
-*
+@DFS - Depth First Search (in adjacency List)
+
+    @Time Complexity: O(V+E)
+    @Space Complexity: O(V+E)
 */
-const { AdjacencyList } = require("../graph-adjacency-list-i");
 
-
-function dfs(adjacencyList, start) {
-    const visited = new Array(adjacencyList.length).fill(false);
-    return dfsHelper(adjacencyList, start, visited);
+function depthFirstSearch(graph, source, n) {
+    const visited = new Array(n).fill(false);
+    return traverse(graph, source, visited);
 }
 
-function dfsHelper(adjacencyList, vertex, visited, traversal=[]) {
-    if(!vertex) return null;
+function traverse(graph, node, visited, traversal=[]) {
+    traversal.push(node);
+    visited[node] = true;
 
-    traversal.push(vertex);
-    visited[vertex] = true;
-
-    for(let i = 0; i < adjacencyList[vertex].length; i++) {
-        let neighbor = adjacencyList[vertex][i];
-
+    for(let neighbor of graph[node]) {
         if(!visited[neighbor]) {
-            dfsHelper(adjacencyList, neighbor, visited, traversal);
+            traverse(graph, neighbor, visited, traversal);
         }
     }
     return traversal;
 }
 
-const graph = new AdjacencyList();
-graph.addVertex(1, 2, 3, 4);
+// directed graph
+let graph = {
+    a: ['c', 'b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: ['d']
+}
+console.log(depthFirstSearch(graph, 'a'));
 
-graph.addEdge(1, 2);
-graph.addEdge(2, 4);
-graph.addEdge(3, 4);
 
-console.log(graph.adjacencyList);
-console.log(dfs(graph.adjacencyList, 1));
+// undirected graph
+graph = {
+    1: [2, 3],
+    2: [1, 4],
+    3: [1],
+    4: [2]
+}
+console.log(depthFirstSearch(graph, 1, 4));
