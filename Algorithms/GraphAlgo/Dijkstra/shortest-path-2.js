499b3a675555c8740a229aeea36d50dd00ad07f4
subroto823 <subroto.sarker.823@gmail.com>
2023-04-01 00:14:05 +0600

    dijkstra - single source shortest path (adjacency list)

--- Algorithms/GraphAlgo/Dijkstra/shortest-path-2.js
+++ Algorithms/GraphAlgo/Dijkstra/shortest-path-2.js
@@ -5,38 +5,33 @@
     Space Complexity: O(V)
 */
 
-// Implement of Dijkstra's single source shortest path algorithm
 function dijkstra(graph, source) {
+    const V = graph.length;
+    const distance = new Array(V).fill(Infinity);
 
+    const Queue = [ source ];
+    distance[source] = 0;
+
+    while(Queue.length) {
+        let currentVertex = Queue.shift();
+
+        for(let [neighbor, weight] of graph[currentVertex]) {
+            let newDistance = (distance[currentVertex] + weight);
+
+            if(newDistance < distance[neighbor]) {
+                distance[neighbor] = distance[currentVertex] + weight;
+                Queue.push(neighbor);
+            }
+        }
+    }
+    return distance;
 }
 
+// adjacent node of vertex i -> [[node, weight], ...]
 let graph = [
-    [0, 2, 7, 10],
-    [2, 0, 0, 2],
-    [7, 0, 0, 1],
-    [10, 2, 1, 0]
+    [[1, 2], [2, 7], [3, 10]],
+    [[0, 2], [3, 2]],
+    [[0, 7], [3, 1]],
+    [[1, 2], [2, 1], [0, 10]]
 ]
 console.log(dijkstra(graph, 2));
-
-graph = [
-    [0, 4, 0, 0, 0, 0, 0, 8, 0],
-    [4, 0, 8, 0, 0, 0, 0, 11, 0],
-    [0, 8, 0, 7, 0, 4, 0, 0, 2],
-    [0, 0, 7, 0, 9, 14, 0, 0, 0],
-    [0, 0, 0, 9, 0, 10, 0, 0, 0],
-    [0, 0, 4, 14, 10, 0, 2, 0, 0],
-    [0, 0, 0, 0, 0, 2, 0, 1, 6],
-    [8, 11, 0, 0, 0, 0, 1, 0, 7],
-    [0, 0, 2, 0, 0, 0, 6, 7, 0]
-];
-
-// console.log(dijkstra(graph, 0));
-
-graph = [
-    [0, 1, 2, 3],
-    [1, 0, 5, 0],
-    [2, 3, 0, 4],
-    [3, 0, 4, 0]
-];
-
-// console.log(dijkstra(graph, 1));
