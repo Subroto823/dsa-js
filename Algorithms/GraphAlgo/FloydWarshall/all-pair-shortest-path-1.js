9c51772279dbaa8ff6785f8f6d427b4f6e7bf28d
subroto823 <subroto.sarker.823@gmail.com>
2023-04-01 12:27:44 +0600

    floyd warshall - all pair shortest path

--- Algorithms/GraphAlgo/FloydWarshall/all-pair-shortest-path-1.js
+++ Algorithms/GraphAlgo/FloydWarshall/all-pair-shortest-path-1.js
@@ -4,8 +4,10 @@ function floydWarshall(matrix) {
     for(let k = 0; k < N; k++) {
         for(let i = 0; i < N; i++) {
             for(let j = 0; j < N; j++) {
-                if((matrix[i][k] + matrix[k][j]) < matrix[i][j]) {
-                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+                let distance = matrix[i][k] + matrix[k][j];
+                
+                if(distance < matrix[i][j]) {
+                    matrix[i][j] = distance
                 }
             }
         }
