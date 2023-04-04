7bc2ad7d82015f4b35b4854e5b3a8980460b4154
subroto823 <subroto.sarker.823@gmail.com>
2023-04-04 15:38:16 +0600

    minimum spanning tree - prim's algo (adj matrix)

--- Algorithms/GraphAlgo/minimum-spanning-tree/prim's-algorithm/mst-1.js
+++ Algorithms/GraphAlgo/minimum-spanning-tree/prim's-algorithm/mst-1.js
@@ -1,11 +1,42 @@
+/*
+Minimum Spanning Tree
+    Prim's Algorithm (with normal queue and sorting - for adjacency matrix)
+
+*/
 function primsMST(graph, source) {
-    
+    const N = graph.length,
+       cost = new Array(N).fill(Infinity),
+       visited = new Array(N).fill(false),
+       queue = [];
+
+    queue.push([source, 0]);
+    cost[source] = 0;
+
+    while(queue.length) {
+        let [ currentNode ] = queue.shift();
+
+        if(visited[currentNode]) continue;
+        visited[currentNode] = true;
+
+        for(let neighbor = 0; neighbor < N; neighbor++) {
+            let neighborCost = graph[currentNode][neighbor];
+
+            if(neighborCost === 0 || visited[neighbor]) continue;
+            if(neighborCost < cost[neighbor]) {
+                queue.push([neighbor, neighborCost]);
+                cost[neighbor] = neighborCost;
+            }
+        }
+        queue.sort((a, b) => a[1] - b[1]);
+    }
+    return cost;
 }
 
 let graph = [
-    [0, 2, 0, 6, 0],
-    [2, 0, 3, 8, 5],
-    [0, 3, 0, 0, 7],
-    [6, 8, 0, 0, 9],
-    [0, 5, 7, 9, 0]
+    [0, 4, 2, 0],
+    [4, 0, 1, 3],
+    [2, 1, 0, 2],
+    [0, 3, 2, 0]
 ];
+
+console.log(primsMST(graph, 0))
