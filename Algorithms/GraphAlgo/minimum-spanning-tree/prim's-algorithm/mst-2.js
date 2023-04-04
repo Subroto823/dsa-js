e2be3d47190c50ddec621d6180b3ea28087f225e
subroto823 <subroto.sarker.823@gmail.com>
2023-04-04 13:07:07 +0600

    minimum spanning tree - prim's algo

--- Algorithms/GraphAlgo/minimum-spanning-tree/prim's-algorithm/mst-2.js
+++ Algorithms/GraphAlgo/minimum-spanning-tree/prim's-algorithm/mst-2.js
@@ -1,37 +1,38 @@
 /*
 Minimum Spanning Tree
-    Prim's Algorithm (With priority queue)
+    Prim's Algorithm (with normal queue and sorting)
 
 */
-const { PriorityQueue } = require('../../_/priority-queue');
-
 function primMST(graph, source) {
     const N = graph.length;
     const cost = new Array(N).fill(Infinity);
     const visited = new Array(N).fill(false);
-    const queue = new PriorityQueue();
+    const queue = [];
 
     cost[source] = 0;
-    queue.enQueue(source, 0);
+    queue.push([source, 0]);
     let ans = 0;
 
     while(queue.length) {
-        let current = queue.deQueue();
+        let current = queue.shift();
+        let [currentNode, currentNodeCost] = current;
 
-        if(visited[current.node]) continue;
+        if(visited[currentNode]) continue;
         
-        visited[current.node] = true;
-        ans += current.priority;
+        visited[currentNode] = true;
+        ans += currentNodeCost;
 
-        for(let [neighbor, weight] of graph[current.node]) {
+        for(let [neighbor, weight] of graph[currentNode]) {
             if(visited[neighbor]) continue;
 
             if(weight < cost[neighbor]) {
-                queue.enQueue(neighbor, weight);
+                queue.push([neighbor, weight]);
                 cost[neighbor] = weight;
             }
         }
+        queue.sort((a, b) => a[1] - b[1]);
     }
+    console.log(cost);
     return ans;
 }
 
@@ -39,6 +40,14 @@ let graph = [
     [[1, 2], [2, 4]],
     [[0, 2], [2, 1], [3, 3]],
     [[0, 4], [1, 1], [3, 2]],
+    [[1, 3], [2, 2]]
+];
+console.log(primMST(graph, 0));
+
+graph = [
+    [[1, 4], [2, 2]],
+    [[0, 4], [2, 1], [3, 3]],
+    [[0, 2], [1, 1], [3, 2]],
     [[1, 3], [2, 2]]
 ];
 console.log(primMST(graph, 0));
