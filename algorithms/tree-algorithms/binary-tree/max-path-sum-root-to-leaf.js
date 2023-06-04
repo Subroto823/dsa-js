04f7dd7b195e609e64726a12533144555a53b473
subroto823 <subroto.sarker.823@gmail.com>
2023-06-04 22:48:30 +0600

    max path sum root to leaf

--- algorithms/tree-algorithms/binary-tree/max-path-sum-root-to-leaf.js
+++ algorithms/tree-algorithms/binary-tree/max-path-sum-root-to-leaf.js
@@ -1,21 +1,30 @@
-const { TreeNode } = require('../../tree-node');
+/*
+Max Path Sum from root to leaf
+        5
+      /   \
+    4      3
+   /  \     \
+  1    2     1
 
-const treeMinValueDfs = (root) => {
-    if(!root) return Infinity;
-    const leftMin = treeMinValueDfs(root.left);
-    const rightMin = treeMinValueDfs(root.right);
-    return Math.min(root.val, leftMin, rightMin);
+result - 11 ( 5 -> 4 -> 2);
+*/
+const { TreeNode } = require('../tree-node');
+
+const maxPathSum = (root) => {
+    if(!root) return -Infinity;
+    if(root.left === null && root.right === null) return root.val;
+    const maxChild = Math.max(maxPathSum(root.left), maxPathSum(root.right));
+    return root.val + maxChild;
 }
 
 
 let tree = new TreeNode(5);
 
-tree.left = new TreeNode(3);
-tree.right = new TreeNode(6);
+tree.left = new TreeNode(4);
+tree.right = new TreeNode(3);
 
-tree.left.left = new TreeNode(2);
-tree.left.right = new TreeNode(4);
-tree.right.right = new TreeNode(8);
+tree.left.left = new TreeNode(1);
+tree.left.right = new TreeNode(2);
+tree.right.right = new TreeNode(1);
 
-console.log(treeMinValue(tree));
-console.log(treeMinValueDfs(tree));
+console.log(maxPathSum(tree));
