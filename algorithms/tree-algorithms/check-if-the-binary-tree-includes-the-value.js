d130976496cc6ddc662ed0578f5c92b52c4b6d05
subroto823 <subroto.sarker.823@gmail.com>
2023-06-04 15:15:28 +0600

    dfs - recursive & iterative (binary tree)

--- algorithms/tree-algorithms/check-if-the-binary-tree-includes-the-value.js
+++ algorithms/tree-algorithms/check-if-the-binary-tree-includes-the-value.js
@@ -1,12 +1,9 @@
 const { TreeNode } = require('./tree-node');
 
-const depthFirstSeach = (root) => {
+const treeIncludes = (root, value) => {
     if(!root) return [];
 
-    const leftValues = depthFirstSeach(root.left);
-    const rightValues = depthFirstSeach(root.right);
-
-    return [root.val, ...leftValues, ...rightValues];
+    
 }
 
 let tree = new TreeNode(5);
@@ -18,4 +15,4 @@ tree.left.left = new TreeNode(2);
 tree.left.right = new TreeNode(4);
 tree.right.right = new TreeNode(8);
 
-console.log(depthFirstSeach(tree));
+console.log(treeIncludes(tree));
--- algorithms/tree-algorithms/dfs.js
+++ algorithms/tree-algorithms/dfs.js
@@ -1,5 +1,6 @@
 const { TreeNode } = require('./tree-node');
 
+// iterative
 const depthFirstSeach = (root) => {
     const stack = [ root ],
         result = [];
@@ -14,6 +15,17 @@ const depthFirstSeach = (root) => {
     return result;
 }
 
+// recursive
+const depthFirstSeachRecursive = (root) => {
+    if(!root) return [];
+
+    const leftValues = depthFirstSeach(root.left);
+    const rightValues = depthFirstSeach(root.right);
+
+    return [root.val, ...leftValues, ...rightValues];
+}
+
+
 let tree = new TreeNode(5);
 
 tree.left = new TreeNode(3);
@@ -24,3 +36,4 @@ tree.left.right = new TreeNode(4);
 tree.right.right = new TreeNode(8);
 
 console.log(depthFirstSeach(tree));
+console.log(depthFirstSeachRecursive(tree));
