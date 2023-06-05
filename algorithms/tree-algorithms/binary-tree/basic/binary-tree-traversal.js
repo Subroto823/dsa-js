ee186350e482cbc42f4212cd887286281f96714d
subroto823 <subroto.sarker.823@gmail.com>
2023-06-05 13:18:02 +0600

    binary tree example

---
+++ algorithms/tree-algorithms/binary-tree/basic/binary-tree-traversal.js
@@ -0,0 +1,37 @@
+class BinaryTreeTraversal {
+    preOrder(root, traversal = []) {
+        if (root === null) return;
+
+        traversal.push(root.value);
+        this.preOrder(root.left, traversal);
+        this.preOrder(root.right, traversal);
+
+        return traversal;
+    }
+
+    inOrder(root, traversal = []) {
+        if (root === null) return;
+
+        this.inOrder(root.left, traversal);
+        traversal.push(root.value);
+        this.inOrder(root.right, traversal);
+
+        return traversal;
+    }
+
+    postOrder(root, traversal = []) {
+        if (root === null) return;
+
+        this.postOrder(root.left, traversal);
+        this.postOrder(root.right, traversal);
+        traversal.push(root.value);
+
+        return traversal;
+    }
+}
+
+const traversal = new BinaryTreeTraversal();
+
+console.log(tree.postOrder(tree.root));
+console.log(tree.inOrder(tree.root));
+console.log(tree.preOrder(tree.root));
---
+++ algorithms/tree-algorithms/binary-tree/binary-tree.js
@@ -0,0 +1,50 @@
+const { Node } = require('../tree-node')
+
+/*
+Example 1:
+
+      1
+   /    \
+  2      3
+ / \    /  \
+4   5  6    7
+
+*/
+
+const tree1 = new Node(1);
+
+tree1.left = new Node(2);
+tree1.right = new Node(3);
+
+tree1.left.left = new Node(4);
+tree1.left.right = new Node(5);
+tree1.right.left = new Node(6);
+tree1.right.right = new Node(7);
+
+
+
+/*
+Example 2:
+
+     5
+   /   \
+  4      3
+ /  \     \
+1    2     1
+
+*/
+
+let tree2 = new TreeNode(5);
+
+tree2.left = new TreeNode(4);
+tree2.right = new TreeNode(3);
+
+tree2.left.left = new TreeNode(1);
+tree2.left.right = new TreeNode(2);
+tree2.right.right = new TreeNode(1);
+
+
+module.exports = {
+    tree1,
+    tree2
+}
