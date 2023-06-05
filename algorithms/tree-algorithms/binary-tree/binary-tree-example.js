116558be7def1175d4cf947243d6bed73903a9e7
subroto823 <subroto.sarker.823@gmail.com>
2023-06-05 13:28:32 +0600

    binary tree example

--- algorithms/tree-algorithms/binary-tree/binary-tree-example.js
+++ algorithms/tree-algorithms/binary-tree/binary-tree-example.js
@@ -1,4 +1,4 @@
-const { Node } = require('./tree-node')
+const { TreeNode } = require('./binary-tree-node')
 
 /*
 Binary Tree
@@ -14,15 +14,15 @@ Example 1:
 
 */
 
-const tree1 = new Node(1);
+const tree1 = new TreeNode(1);
 
-tree1.left = new Node(2);
-tree1.right = new Node(3);
+tree1.left = new TreeNode(2);
+tree1.right = new TreeNode(3);
 
-tree1.left.left = new Node(4);
-tree1.left.right = new Node(5);
-tree1.right.left = new Node(6);
-tree1.right.right = new Node(7);
+tree1.left.left = new TreeNode(4);
+tree1.left.right = new TreeNode(5);
+tree1.right.left = new TreeNode(6);
+tree1.right.right = new TreeNode(7);
 
 
 
