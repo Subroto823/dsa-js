d551f6d7ed56efb57f97bd2d3fda384d25654fdd
subroto823 <subroto.sarker.823@gmail.com>
2023-06-16 12:08:56 +0600

    iterative dfs (in binary tree)

--- algorithms/tree-algorithms/binary-tree/basic/dfs.js
+++ algorithms/tree-algorithms/binary-tree/basic/dfs.js
@@ -1,6 +1,6 @@
-const { TreeNode } = require('../../tree-node');
+const { tree1 } = require('../binary-tree-example');
 
-// iterative
+// iterative dfs
 const depthFirstSeach = (root) => {
     const stack = [ root ],
         result = [];
@@ -15,25 +15,4 @@ const depthFirstSeach = (root) => {
     return result;
 }
 
-// recursive
-const depthFirstSeachRecursive = (root) => {
-    if(!root) return [];
-
-    const leftValues = depthFirstSeach(root.left);
-    const rightValues = depthFirstSeach(root.right);
-
-    return [root.val, ...leftValues, ...rightValues];
-}
-
-
-let tree = new TreeNode(5);
-
-tree.left = new TreeNode(3);
-tree.right = new TreeNode(6);
-
-tree.left.left = new TreeNode(2);
-tree.left.right = new TreeNode(4);
-tree.right.right = new TreeNode(8);
-
-console.log(depthFirstSeach(tree));
-console.log(depthFirstSeachRecursive(tree));
+console.log(depthFirstSeach(tree1));
