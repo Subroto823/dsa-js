c0dde3f881ce9a3aaaa19f848a2036789dfb001f
subroto823 <subroto.sarker.823@gmail.com>
2023-07-19 02:48:34 +0600

    random numbers array

--- algorithms/_basic-algorithms/sorting-algorithms/_helper-functions/random-numbers-array.js
+++ algorithms/_basic-algorithms/sorting-algorithms/_helper-functions/random-numbers-array.js
@@ -1,16 +1,10 @@
 function randomArray(length, max = length) {
-    // ~~ is a faster substitute for Math.floor() for positive numbers
-    return Array.from({ length: length }, () => ~~(Math.random() * max));
+    return Array.from({ length: length }, () => {
+        return Math.floor(Math.random() * max);
+    })
 }
 
 function randomNumbers(length, min = 0, max = length) {
-    if(arguments.length === 2) {
-        if(min > 0) {
-            max = min;
-            min = 0;
-        }
-    }
-
     return Array.from({ length: length }, () => {
         return randomRange(min, max);
     });
@@ -20,9 +14,8 @@ function randomRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(randomNumbers(10, 10));
-
 module.exports = {
     randomArray,
-    randomNumbers
+    randomNumbers,
+    randomRange
 }
