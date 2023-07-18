cfe539ee5b4cf92c0657549b749dd7583b4baaf5
subroto823 <subroto.sarker.823@gmail.com>
2023-07-19 03:10:24 +0600

    insertion sort

--- algorithms/_basic-algorithms/sorting-algorithms/3-insertion-sort-1.js
+++ algorithms/_basic-algorithms/sorting-algorithms/3-insertion-sort-1.js
@@ -1,21 +1,21 @@
 /**
- * @Insertion Sort
+ # Insertion Sort
  *
- * @Time Complexity - O(n^2)
- * @stable algorithm.
+ * Time Complexity - O(n^2)
+ * Stable algorithm.
  * 
  */
 
 const insertionSort = (arr) => {
-    for(let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         let numberToInsert = arr[i];
         let j = i - 1;
 
-        while(j >= 0 && arr[j] > numberToInsert) {
-            arr[j+1] = arr[j];
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1] = numberToInsert;
+        arr[j + 1] = numberToInsert;
     }
     return arr;
 }
