dba8bd3559563042377a53b75d3e307bb8ab5974
subroto823 <subroto.sarker.823@gmail.com>
2023-07-18 11:31:23 +0600

    random numbers

--- helpers/random-numbers.js
+++ helpers/random-numbers.js
@@ -1,16 +1,10 @@
 function randomArray(length, max = length) {
-    // ~~ is a faster substitute for Math.floor() for positive numbers
-    return Array.from({ length: length }, () => ~~(Math.random() * max));
+    return Array.from({ length: length }, () => {
+        return Math.floor(Math.random() * max);
+    });
 }
 
 function randomNumbers(length, min = 0, max = length) {
-    if(arguments.length === 2) {
-        if(min > 0) {
-            max = min;
-            min = 0;
-        }
-    }
-
     return Array.from({ length: length }, () => {
         return randomRange(min, max);
     });
