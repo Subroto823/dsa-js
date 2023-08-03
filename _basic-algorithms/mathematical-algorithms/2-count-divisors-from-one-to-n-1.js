c072906f872c48ef2eb28097c0386d8478f25e8f
subroto823 <subroto.sarker.823@gmail.com>
2023-08-04 05:59:01 +0600

    count divisors from one to n

---
+++ _basic-algorithms/mathematical-algorithms/2-count-divisors-from-one-to-n-1.js
@@ -0,0 +1,31 @@
+function countDivisorsInRange(n) {
+    let divisors = []
+    for (let i = 1; i <= n; i++) {
+        divisors.push(countDivisors(i))
+    }
+
+    return divisors;
+}
+
+function countDivisors(number) {
+    let count = 0;
+
+    for (let i = 1; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            if (number / i === i) {
+                count += 1;
+            } else {
+                count += 2; 
+            }
+        }
+    }
+
+    return count;
+}
+
+console.log(countDivisorsInRange(10));
+
+/*
+The downside of this approach is that it is inefficient for large values of n. The main reason for this inefficiency is the repeated calculation of divisors for each number from 1 to n.
+
+In the countDivisorsInRange function, it calls the countDivisors function for each number in the range from 1 to n, resulting in multiple calculations for the same numbers. This leads to unnecessary redundant computations and significantly increases the time complexity. */
