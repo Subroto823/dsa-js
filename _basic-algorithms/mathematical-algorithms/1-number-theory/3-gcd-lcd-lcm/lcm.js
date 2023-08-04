7cde80a6f3574a6b2428e9cd0b056d464a77baea
subroto823 <subroto.sarker.823@gmail.com>
2023-08-05 01:38:26 +0600

    lcm - least common multiple

--- _basic-algorithms/mathematical-algorithms/1-number-theory/3-gcd-lcd-lcm/lcm.js
+++ _basic-algorithms/mathematical-algorithms/1-number-theory/3-gcd-lcd-lcm/lcm.js
@@ -1,12 +1,15 @@
+/*
+The Least Common Multiple (LCM) is the smallest positive integer that is divisible by both of the given positive integers without leaving any remainder. */
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
 function gcd(a, b) {
     if (b === 0) {
         return a;
     }
     return gcd(b, a % b);
-}
-
-function lcm(a, b) {
-    return (a * b) / gcd(a, b);
 }
 
 console.log(lcm(6, 8));
