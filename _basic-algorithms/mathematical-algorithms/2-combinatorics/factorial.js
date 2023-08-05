d1cfb96c31521813a0d5efb7320e5edead5eeb6f
subroto823 <subroto.sarker.823@gmail.com>
2023-08-06 00:31:34 +0600

    factorical

--- _basic-algorithms/mathematical-algorithms/2-combinatorics/factorial.js
+++ _basic-algorithms/mathematical-algorithms/2-combinatorics/factorial.js
@@ -1,13 +1,12 @@
 /*
  # Factorial
- * Time Complexity O(n)
- * 
+    # Time Complexity O(n)
  */
 
 const factorial = (num) => {
     let product = 1;
-    for(let i = num; i > 1; i--) {
-        product = product * i;
+    for(let i = 2; i <= num; i++) {
+        product *= i;
     }
     return product;
 }
