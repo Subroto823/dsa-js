2c2e7ecb5450c747a550bcc044325c996957f406
subroto823 <subroto.sarker.823@gmail.com>
2023-08-05 14:49:55 +0600

    modular exponentiation

--- _basic-algorithms/mathematical-algorithms/modulo-arithmetic/4-modular-exponentiation-1.js
+++ _basic-algorithms/mathematical-algorithms/modulo-arithmetic/4-modular-exponentiation-1.js
@@ -2,12 +2,12 @@
 # binary exponentiation with modular arithmetic
     # (a^b) % m = ((a % m)^b) % m
 */
-function binpow(a, b, m) {
+function modularExponentiation(a, b, m) {
     return ((a % m) ** b) % m;
 }
 
-console.log(binpow(10, 2, 7));
-console.log(binpow(2, 7, 10));
+console.log(modularExponentiation(10, 2, 7));
+console.log(modularExponentiation(2, 7, 10));
 
 /*
 Downsides of this one-liner implementation:
---
+++ _basic-algorithms/mathematical-algorithms/modulo-arithmetic/4-modular-exponentiation-3.js
@@ -0,0 +1,19 @@
+// Iterative approach
+function modularExponentiation(a, b, m) {
+    a = a % m;
+    let result = 1;
+
+    while (b > 0) {
+        if (b % 2 === 1) {
+            result = (result * a) % m;
+        }
+
+        a = (a * a) % m;
+        b = Math.floor(b / 2);
+    }
+
+    return result;
+}
+
+const result = modularExponentiation(2, 7, 10);
+console.log(result); // Output: 8 (2^7 % 10 = 128 % 10 = 8)
