44ce75722e7c7016523fbdae6e18070d18227bb1
subroto823 <subroto.sarker.823@gmail.com>
2023-08-10 14:24:43 +0600

    count divisors

---
+++ algorithms/02-mathematical/1-number-theory/2-divisors/1-count-divisors-1.js
@@ -0,0 +1,33 @@
+/*
+# Count Divisors
+
+In mathematics, a divisor of an integer is a whole number that divides the given integer evenly without leaving a remainder.
+
+For example:
+
+    Divisors of 10 are 1, 2, 5, and 10, as they divide 10 without leaving a remainder.
+
+    Divisors of 12 are 1, 2, 3, 4, 6, and 12
+
+    Divisors of 7 are 1 and 7, as they are the only integers that divide 7 without leaving a remainder.
+
+The number 1 is considered a divisor of all integers, and each integer is also a divisor of itself. The largest divisor of any integer is the number itself. 
+
+# Time Complexity: O(n)
+*/
+
+function countDivisors(number) {
+    let count = 0;
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countDivisors(4));
+console.log(countDivisors(12));
+console.log(countDivisors(18));
