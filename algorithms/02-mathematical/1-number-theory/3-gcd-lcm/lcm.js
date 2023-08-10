643b8a412e53bdcaa4498d4f48f89043a5497122
subroto823 <subroto.sarker.823@gmail.com>
2023-08-10 15:06:48 +0600

    lcm (least common multiple)

--- algorithms/02-mathematical/1-number-theory/3-gcd-lcm/lcm.js
+++ algorithms/02-mathematical/1-number-theory/3-gcd-lcm/lcm.js
@@ -1,5 +1,13 @@
 /*
-The Least Common Multiple (LCM) is the smallest positive integer that is divisible by both of the given positive integers without leaving any remainder. */
+# LCM (Least Common Multiple )
+
+    # LCM is the smallest positive integer that is divisible by both of the given positive integers without leaving any remainder. 
+    
+    # The approach is to use the relationship between GCD and LCM:
+        LCM(a, b) = (a * b) / GCD(a, b)
+
+    # Time Complexity: O(log(min(a, b)))
+*/
 
 function lcm(a, b) {
     return (a * b) / gcd(a, b);
@@ -13,3 +21,10 @@ function gcd(a, b) {
 }
 
 console.log(lcm(6, 8));
+
+/*
+LCM(6, 8)
+    = (6 * 8) / GCD(6, 8)
+    = 48 / 2
+    = 24
+*/
