8a37ffd6f4d947e7bdb348663dbeb33491b70cdc
subroto823 <subroto.sarker.823@gmail.com>
2023-09-24 02:57:00 +0600

    prim's algorithm (with priority queue)

--- algorithms/09-graph/minimum-spanning-tree/prim's-algorithm-2.js
+++ algorithms/09-graph/minimum-spanning-tree/prim's-algorithm-2.js
@@ -5,9 +5,9 @@ Prim's Algorithm (With priority queue)
 Time Complexity: O(Elog(V))
 */
 
-const { MinPriorityQueue,} = require('../../../__helpers/min-priority-queue');
+const { MinPriorityQueue, } = require('../../../__helpers/min-priority-queue');
 
-var primsMST = function(graph) {
+var primsMST = function (graph) {
     let N = graph.length;
     const seen = new Array(N).fill(false);
     const queue = new MinPriorityQueue();
@@ -15,24 +15,26 @@ var primsMST = function(graph) {
     queue.enqueue(0, 0);
     let ans = 0;
 
-    while(N--) {
+    while (N) {
         if (queue.isEmpty()) {
             console.log("No MST :(");
             return 0;
         }
 
-        let current = queue.dequeue();
-        let {element: node, priority: cost} = current;
+        let { element: node, priority: cost } = queue.dequeue();
 
+        if (seen[node]) continue;
         seen[node] = true;
         ans += cost;
 
-        for(let [neighbor, weight] of graph[node]) {
-            if(seen[neighbor]) continue;
+        for (let [neighbor, weight] of graph[node]) {
+            if (seen[neighbor]) continue;
             queue.enqueue(neighbor, weight);
         }
+
+        N--;
     }
-    
+
     return ans;
 }
 
@@ -49,38 +51,48 @@ Input:
     ]
 */
 
-let graph = [
+const graph1 = [
     [[1, 2], [2, 4]],
     [[0, 2], [2, 1], [3, 3]],
     [[0, 4], [1, 1], [3, 2]],
     [[1, 3], [2, 2]]
 ];
-console.log(primsMST(graph));
 
-graph = [
+const graph2 = [
     [[1, 4], [2, 2]],
     [[0, 4], [2, 1], [3, 3]],
     [[0, 2], [1, 1], [3, 2]],
     [[1, 3], [2, 2]]
 ];
-console.log(primsMST(graph));
 
-graph = [
+const graph3 = [
     [[1, 2], [2, 3]],
     [[3, 4], [0, 2]],
     [[0, 3], [3, 1], [4, 3]],
     [[1, 4], [2, 1], [4, 2]],
     [[3, 2], [2, 3]]
-]
-console.log(primsMST(graph));
+];
 
-graph = [
+const graph4 = [
+    [[1, 4], [2, 13], [3, 7], [4, 7]],
+    [[0, 4], [2, 9], [3, 3], [4, 7]],
+    [[0, 13], [1, 9], [3, 10], [4, 14]],
+    [[0, 7], [1, 3], [2, 10], [4, 4]],
+    [[0, 7], [1, 7], [2, 14], [3, 4]]
+];
+
+const graph5 = [
     [[1, 2]],
     [[0, 2]],
     [[3, 4]],
     [[2, 4]]
-]
-console.log(primsMST(graph));
+];
+
+console.log(primsMST(graph1));
+console.log(primsMST(graph2));
+console.log(primsMST(graph3));
+console.log(primsMST(graph4));
+console.log(primsMST(graph5));
 console.log();
 
 
@@ -89,7 +101,7 @@ What if we also want the mst?
 */
 
 
-var primsMST = function(graph) {
+var primsMST = function (graph) {
     let N = graph.length;
     const seen = new Array(N).fill(false);
     const queue = new MinPriorityQueue();
@@ -97,54 +109,32 @@ var primsMST = function(graph) {
     queue.enqueue(0, 0);
     const mst = [];
 
-    while(N--) {
+    while (N) {
         if (queue.isEmpty()) {
             console.log("No MST :(");
             return [];
         }
 
         const { element: node } = queue.dequeue();
+
+        if (seen[node]) continue;
         seen[node] = true;
         mst.push(node);
 
-        for(let [neighbor, weight] of graph[node]) {
-            if(seen[neighbor]) continue;
+        for (let [neighbor, weight] of graph[node]) {
+            if (seen[neighbor]) continue;
             queue.enqueue(neighbor, weight);
         }
+
+        N--;
     }
 
-   return mst;
+    return mst;
 }
 
-graph = [
-    [[1, 2], [2, 4]],
-    [[0, 2], [2, 1], [3, 3]],
-    [[0, 4], [1, 1], [3, 2]],
-    [[1, 3], [2, 2]]
-];
-console.log(primsMST(graph));
-
-graph = [
-    [[1, 4], [2, 2]],
-    [[0, 4], [2, 1], [3, 3]],
-    [[0, 2], [1, 1], [3, 2]],
-    [[1, 3], [2, 2]]
-];
-console.log(primsMST(graph));
-
-graph = [
-    [[1, 2], [2, 3]],
-    [[3, 4], [0, 2]],
-    [[0, 3], [3, 1], [4, 3]],
-    [[1, 4], [2, 1], [4, 2]],
-    [[3, 2], [2, 3]]
-]
-console.log(primsMST(graph));
-
-graph = [
-    [[1, 2]],
-    [[0, 2]],
-    [[3, 4]],
-    [[2, 4]]
-]
-console.log(primsMST(graph));
+console.log(primsMST(graph1));
+console.log(primsMST(graph2));
+console.log(primsMST(graph3));
+console.log(primsMST(graph4));
+console.log(primsMST(graph5));
+console.log();
--- algorithms/09-graph/minimum-spanning-tree/prim's-algorithm-3.js
+++ algorithms/09-graph/minimum-spanning-tree/prim's-algorithm-3.js
@@ -8,6 +8,7 @@ const { MinPriorityQueue } = require('../../../__helpers/min-priority-queue');
 var primsMST = function(graph) {
     const N = graph.length;
     const parent = new Array(N).fill(-1);
+    const cost = new Array(N).fill(Infinity);
     const seen = new Array(N).fill(false);
     const queue = new MinPriorityQueue();
 
@@ -16,11 +17,16 @@ var primsMST = function(graph) {
 
     while(queue.size()) {
         const { element: node } = queue.dequeue();
+
+        if (seen[node]) continue;
         seen[node] = true;
 
         for(let [neighbor, weight] of graph[node]) {
             if(seen[neighbor]) continue;
+            if(weight >= cost[neighbor]) continue
+
             parent[neighbor] = node;
+            cost[neighbor] = weight;
             queue.enqueue(neighbor, weight);
         }
     }
@@ -32,42 +38,53 @@ var primsMST = function(graph) {
             console.log("No MST :(");
             return [];
         }
+
         mst.push([parent[i], i]);
     }
 
-   return mst;
+    return mst;
 }
 
-let graph = [
+const graph1 = [
     [[1, 2], [2, 4]],
     [[0, 2], [2, 1], [3, 3]],
     [[0, 4], [1, 1], [3, 2]],
     [[1, 3], [2, 2]]
 ];
-console.log(primsMST(graph));
 
-graph = [
+const graph2 = [
     [[1, 4], [2, 2]],
     [[0, 4], [2, 1], [3, 3]],
     [[0, 2], [1, 1], [3, 2]],
     [[1, 3], [2, 2]]
 ];
-console.log(primsMST(graph));
 
-graph = [
+const graph3 = [
     [[1, 2], [2, 3]],
     [[3, 4], [0, 2]],
     [[0, 3], [3, 1], [4, 3]],
     [[1, 4], [2, 1], [4, 2]],
     [[3, 2], [2, 3]]
-]
-console.log(primsMST(graph));
+];
 
-graph = [
+const graph4 = [
+    [[1, 4], [2, 13], [3, 7], [4, 7]],
+    [[0, 4], [2, 9], [3, 3], [4, 7]],
+    [[0, 13], [1, 9], [3, 10], [4, 14]],
+    [[0, 7], [1, 3], [2, 10], [4, 4]],
+    [[0, 7], [1, 7], [2, 14], [3, 4]]
+];
+
+const graph5 = [
     [[1, 2]],
     [[0, 2]],
     [[3, 4]],
     [[2, 4]]
-]
-console.log(primsMST(graph));
+];
+
+console.log(primsMST(graph1));
+console.log(primsMST(graph2));
+console.log(primsMST(graph3));
+console.log(primsMST(graph4));
+console.log(primsMST(graph5));
 console.log();
