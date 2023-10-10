928828c09c4a6e5562fb31cbd290972cd65de23c
subroto823 <subroto.sarker.823@gmail.com>
2023-10-10 16:00:14 +0600

    longest substring length with k distinct characters

--- algorithms/13-adhoc-techniques/the-sliding-window/longest-substring-length-with-k-distinct-characters.js
+++ algorithms/13-adhoc-techniques/the-sliding-window/longest-substring-length-with-k-distinct-characters.js
@@ -1,31 +1,32 @@
 /**
- # Find length of the longest substring of k distinct characters
- * 
+ * Find length of the longest substring of k distinct characters.
  */
 
-function findLength(str, k) {
+var findLength = function(str, k) {
     const map = new Map();
-    let maxWindowSize = -Infinity;
-    let windowStart = 0;
+    let result = -Infinity;
+    let start = 0;
 
-    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-        let charater = str[windowEnd];
-        map.set(charater, (map.get(charater) || 0) + 1);
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        map.set(char, (map.get(char) || 0) + 1);
 
-        while (map.size > k) {
-            let startChar = str[windowStart];
-            map.set(startChar, map.get(startChar) - 1);
-
-            if (map.get(startChar) === 0) {
-                map.delete(startChar);
-            }
-            windowStart++;
+        if (map.size > k) {
+            let startChar = str[start];
+            while(startChar === str[start]) start++;
+            map.delete(startChar);
         }
-        maxWindowSize = Math.max(maxWindowSize, windowEnd - windowStart + 1);
+
+        result = Math.max(result, i - start + 1);
     }
-    return maxWindowSize;
+
+    return result;
 }
 
 let str = "AAAHHIBCCD"
 let k = 2;
+console.log(findLength(str, k));
+
+str = "AAAHHIBBBCCDD"
+k = 3;
 console.log(findLength(str, k));
