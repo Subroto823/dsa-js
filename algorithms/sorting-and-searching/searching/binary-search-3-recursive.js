b85f3c1dc1b38ecfc5b0d43d2cb362edd8f59c0b
subroto823 <subroto.sarker.823@gmail.com>
2023-10-13 16:27:35 +0600

    binary search

--- algorithms/sorting-and-searching/searching/binary-search-3-recursive.js
+++ algorithms/sorting-and-searching/searching/binary-search-3-recursive.js
@@ -1,25 +1,26 @@
-/*
-# Binary Search (Recursive)
-    # Time complexity O(logn)
-*/ 
+/**
+ * Binary Search (Recursive)
+ * Time complexity O(logn)
+ */
 
-const recursiveBinarySearch = (arr, target) => {
-    const search = (start, end) => {
-        if(start > end) return -1;
+const recursiveBinarySearch = function (arr, target) {
+    const search = function (l, r) {
+        if (l > r) return -1;
 
-        let mid = Math.floor((start + end) / 2);  
+        let mid = l + ((r - l) >> 1)
 
-        if(target === arr[mid]) {
+        if (target === arr[mid]) {
             return mid;
-        }
-        else if(target < arr[mid]) {
-           return search(start, mid - 1);
+        } else if (target < arr[mid]) {
+            return search(l, mid - 1);
         } else {
-           return search(mid + 1, end);
+            return search(mid + 1, r);
         }
     }
 
     return search(0, arr.length - 1);
 }
 
-console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 4));
+
+let nums = [-5, 2, 4, 6, 10], target = 6;
+console.log(recursiveBinarySearch(nums, target));
