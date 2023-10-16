39f071168454ab68c1783219732b13ffe6bdfdc7
subroto823 <subroto.sarker.823@gmail.com>
2023-10-17 00:32:10 +0600

    Hash Table

--- data-structures/hash-table/hash-table.js
+++ data-structures/hash-table/hash-table.js
@@ -8,78 +8,133 @@ class HashTable {
         this.size = 0;
     }
 
+    getSize() {
+        return this.size;
+    }
+
+    clear() {
+        this.table = new Array(this.table.length);
+        this.size = 0;
+    }
+
     _hash(key) {
+        const prime = 31;
         let total = 0;
-        for(let i = 0; i < key.length; i++) {
-            total += key.charCodeAt(i);
+        key = key.toString();
+
+        for (let i = 0; i < Math.min(key.length, 50); i++) {
+            const charValue = key.charCodeAt(i) - 96;
+            total = (total * prime + charValue) % this.table.length;
         }
-        return total % this.size;
+
+        return total;
     }
 
     set(key, value) {
-        const index = this.hash(key);
+        const index = this._hash(key);
+
+        if (!this.table[index]) this.table[index] = [];
+
         const bucket = this.table[index];
-        if(!bucket) {
-            this.table[index] = [[key, value]]
+        const sameKeyItem = bucket.find(item => item[0] === key);
+
+        if (sameKeyItem) {
+            sameKeyItem[1] = value;
         } else {
-            const sameKeyItem = bucket.find(item => item[0]=== key)
-            if(sameKeyItem) {
-                sameKeyItem[1] = value;
-            } else {
-                bucket.push([key, value]);
-            }
+            bucket.push([key, value]);
+            this.size++;
         }
     }
 
     get(key) {
-        const index = this.hash(key);
+        const index = this._hash(key);
         const bucket = this.table[index];
 
-        if(bucket) {
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if(sameKeyItem) {
-                return sameKeyItem[1];
+        if (bucket) {
+            for (const entry of bucket) {
+                if (entry[0] === key) {
+                    return entry[1];
+                }
             }
         }
+
         return undefined;
     }
 
     remove(key) {
-        const index = this.hash(key);
+        const index = this._hash(key);
         const bucket = this.table[index];
 
-        if(bucket) {
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if(sameKeyItem) {
-                return bucket.splice(bucket.indexOf(sameKeyItem), 1);
+        if (bucket) {
+            for (let i = 0; i < bucket.length; i++) {
+                if (bucket[i][0] === key) {
+                    bucket.splice(i, 1);
+                    this.size--;
+                    return;
+                }
             }
         }
     }
 
-    display() {
-        for(let i = 0; i < this.table.length; i++) {
-            if(this.table[i]) {
-                console.log(i, this.table[i]);
+    entries() {
+        const entries = [];
+
+        for (const items of this.table) {
+            if (items) {
+                for (const item of items) {
+                    entries.push(item);
+                }
             }
         }
+        
+        return entries;
+    }
+
+    keys() {
+        const keys = [];
+
+        for (const items of this.table) {
+            if (items) {
+                for (const item of items) {
+                    keys.push(item[0]);
+                }
+            }
+        }
+        
+        return keys;
+    }
+
+    values() {
+        const values = [];
+
+        for (const items of this.table) {
+            if (items) {
+                for (const item of items) {
+                    values.push(item[1]);
+                }
+            }
+        }
+        
+        return values;
     }
 }
 
+function main() {
+    const table = new HashTable(50);
 
-const table = new HashTable(50);
+    table.set("name", "BatMan");
+    table.set("age", 25);
+    table.set("mane", "SpiderMan");
 
-table.set("name", "Bruce");
-table.set("age", 25);
-table.display();
+    console.log(table.get("name"));
+    console.log(table.get("mane"));
 
-console.log();
+    console.log(table.entries());
 
-// our hashing function is so simple. This will cause a collision and we will loose the data "Bruce"
-table.set("mane", "Clark");
-table.display();
+    table.remove('age');
+    console.log(table.entries());
+}
 
-console.log(table.get("name"));
-console.log(table.get("mane"));
+if (require.main === module) main();
 
-table.set("name", "Diana");
-table.display();
+module.exports = HashTable;
