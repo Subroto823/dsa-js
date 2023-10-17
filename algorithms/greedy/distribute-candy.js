2adc1d0fe6d468c89f87cb629da88d375df13581
subroto823 <subroto.sarker.823@gmail.com>
2023-10-17 23:27:01 +0600

    distribute candy

--- algorithms/greedy/distribute-candy.js
+++ algorithms/greedy/distribute-candy.js
@@ -2,9 +2,8 @@
 Problem Discussion
 
 N kids stand in a line, each having an integer rating. We distribute candies following:
-
-    # Each kid gets at least 1 candy
-    # Kids with higher ratings than their neighbours get more candies.
+    - Each kid gets at least 1 candy
+    - Kids with higher ratings than their neighbours get more candies.
 
 Find the minimum candies required.
 
@@ -16,12 +15,12 @@ Constraints:
     * 1 <= N <= 1e5
 */
 
-function solve(ratings) {
-    let data = ratings.map((elem, i) => [elem, i]);
+const candy = function (ratings) {
+    const data = ratings.map((rating, i) => [rating, i]);
     data.sort((a, b) => a[0] - b[0]);
 
-    let n = ratings.length;
-    let candies = new Array(n).fill(1);
+    const n = ratings.length;
+    const candies = new Array(n).fill(1);
 
     for(let [_, i] of data) {
         if(i > 0 && ratings[i] > ratings[i - 1]) {
@@ -32,7 +31,9 @@ function solve(ratings) {
             candies[i] = Math.max(candies[i], candies[i + 1] + 1);
         }
     }
+
     return candies.reduce((acc, elem) => acc + elem, 0);
 }
 
-console.log(solve([1, 3, 7, 1]));
+let candies = [1, 3, 7, 1];
+console.log(candy(candies));
