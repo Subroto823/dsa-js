36f6bc2ee5ca0ac1611d5fef5823389677084990
subroto823 <subroto.sarker.823@gmail.com>
2023-11-01 12:59:57 +0600

    is prime

---
+++ algorithms/math/number-theory/prime-numbers/is-prime.js
@@ -0,0 +1,77 @@
+/**
+ * Primality Test
+ * 
+ * Naive Approach
+ * Time Complexity O(n)
+ */
+
+var isPrime = function(number) {
+    if(number < 2) return false;
+
+    for(let i = 2; i < number; i++) {
+        if(number % i === 0 ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(2504987));
+console.log(isPrime(15));
+console.log();
+
+
+
+/**
+ * Efficient Approach
+ * 
+ * Idea:
+ * Integers larger than the square root do not need to be checked because, whenever "number = a*b", one of the two factors "a" and "b" is less than or equal to the square root of "n"
+ * 
+ * Time Complexity O(sqrt(n))
+*/
+
+var isPrime =function(number) {
+    if(number < 2) return false;
+
+    for(let i = 2; i <= Math.sqrt(number) ; i++) {
+        if(number % i === 0 ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(2504987));
+console.log(isPrime(17));
+console.log();
+
+
+/**
+ * More Efficient
+ * Time Complexity: O(sqrt(n))
+ */
+var isPrime = function(n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 === 0 || n % 3 === 0) {
+        return false;
+    }
+
+    let i = 5;
+    while (i * i <= n) {
+        if (n % i === 0 || n % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
+    }
+
+    return true;
+}
+
+console.log(isPrime(999998727899999));
+console.log(isPrime(17));
+console.log();
