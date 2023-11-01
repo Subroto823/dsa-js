657ee538b0ba4793c2136312f6d3ca9add73d1ec
subroto823 <subroto.sarker.823@gmail.com>
2023-11-01 13:11:49 +0600

    prime numbers within range

--- algorithms/math/number-theory/prime-numbers/prime-numbers-within-a-range.js
+++ algorithms/math/number-theory/prime-numbers/prime-numbers-within-a-range.js
@@ -4,20 +4,13 @@
  * Naive Approach
  * Time Complexity:  O(n * sqrt(n))
  */
-var isPrime = function(number) {
-    if (number <= 1) return false;
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) return false;
-    }
+const isPrime = require('./is-prime');
 
-    return true;
-}
+var findPrimesInRange = function (n) {
+    const primes = [];
 
-function findPrimesInRange(n) {
-    let primes = [];
-
-    for (let num = 0; num <= n; num++) {
+    for(let num = 0; num <= n; num++) {
         if (isPrime(num)) {
             primes.push(num);
         }
@@ -29,9 +22,11 @@ function findPrimesInRange(n) {
 let primesInRange = findPrimesInRange(10);
 console.log(primesInRange);
 
+
 /*
 While this approach works, it has a few downsides:
 
-# Efficiency: The code checks each number from 0 to n for primality by testing its divisibility by all integers up to its square root. This results in a time complexity of approximately O(n √n), which can become slow for larger values of n.
+- Efficiency: The code checks each number from 0 to n for primality by testing its divisibility by all integers up to its square root. This results in a time complexity of approximately O(n √n), which can become slow for larger values of n.
 
-# Repetitive Primality Testing: The isPrime function performs primality testing for every number in the range, even for those that have already been identified as prime. This repetitive testing wastes computation. */
+- Repetitive Primality Testing: The isPrime function performs primality testing for every number in the range, even for those that have already been identified as prime. This repetitive testing wastes computation. 
+*/
