353fc50d4e2a689a903ae4dc6a7420da79ddba23
subroto823 <subroto.sarker.823@gmail.com>
2023-11-03 15:59:32 +0600

    combinations

--- algorithms/math/combinatorics/calculate-combinations-3.js
+++ algorithms/math/combinatorics/calculate-combinations-3.js
@@ -1,39 +1,32 @@
 /*
 Pascal's Triangle is a triangular array of binomial coefficients. Each element in the triangle represents the value of nCr, where n is the row number, and r is the position of the element within the row. Pascal's Triangle can be efficiently constructed using dynamic programming, and it allows us to find nCr without explicitly calculating factorials. */
 
-function generatePascalsTriangle(rows) {
-    const triangle = [];
 
-    for (let i = 0; i < rows; i++) {
-        const row = [];
-
-        for (let j = 0; j <= i; j++) {
-            if (j === 0 || j === i) {
-                row.push(1);
-            } else {
-                row.push(triangle[i - 1][j - 1] + triangle[i - 1][j]);
-            }
-        }
-
-        triangle.push(row);
-    }
-
-    return triangle;
-}
-
-function nCrUsingPascalsTriangle(n, r) {
-    if (n < r) {
-        throw new Error("n should be greater than or equal to r.");
-    }
-
-    const pascalsTriangle = generatePascalsTriangle(n + 1);
+const nCr = function(n, r, pascalsTriangle) {
     return pascalsTriangle[n][r];
 }
 
-const n = 5;
-const r = 2;
-const result = nCrUsingPascalsTriangle(n, r); // 5C2 = 10
-console.log(result);
+const generatePascalsTriangle = function (numRows) {
+    const triangle = new Array(numRows)
+        .fill()
+        .map((_, i) => new Array(i + 1).fill(1));
+
+    for (let i = 2; i < numRows; i++) {
+        for (let j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    return triangle;
+};
+
+
+let n = 5;
+let triangle = generatePascalsTriangle(n+1);
+console.log(nCr(n, 2, triangle));
+console.log(nCr(n, 3, triangle));
+console.log(nCr(n, 4, triangle));
+console.log(nCr(n, 5, triangle));
 
 /*
 
